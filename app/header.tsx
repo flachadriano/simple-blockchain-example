befb39d2267a08ac614165a36a931fd5471e5979
flachadriano <adriano.araujo@pulsati.com.br>
2025-10-06 00:52:21 -0300

    style hedaer

--- app/header.tsx
+++ app/header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export function Header() {
   return (
-    <Box sx={{ margin: '10px' }}>
+    <Box sx={{ padding: '10px', backgroundColor: '#ddd', color: '#000', fontWeight: 'bold', fontSize: '18px' }}>
       <Link to='/'>Hash</Link>
       <Link style={{ marginLeft: '20px' }} to='/block'>Block</Link>
       <Link style={{ marginLeft: '20px' }} to='/blockchain'>Blockchain</Link>
